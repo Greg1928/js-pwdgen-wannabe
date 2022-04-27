e42763d6d3feb58136d3d1a64e26cc2515b88543
Greg1928 <gregoriosortino@protonmail.com>
2022-04-27 15:52:29 +0200

    added console password generator

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/* input */
+const nameP = (prompt("Enter your name"));
+const surname = (prompt("Enter your surname"));
+const color = (prompt("enter your favourite color"));
+
+/* data manipulation*/
+const password = nameP + surname + color + 21;
+
+/* output */
+console.log(password);
